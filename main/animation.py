6e3ac4d53a25b524d2339036c271eb689b4c7d22
Shashank S K <shashank@mailinator.org>
2021-08-14 23:04:59 +0530

    Removed pos_update and added functionality to animate().

--- main/animation.py
+++ main/animation.py
@@ -31,34 +31,18 @@ def spring(position):
     return -k*(distance - length)
 
 
-#@jit(fastmath=True)
-def pos_update(position, angle):
-    F = spring(position)
-    for i in range(len(pos)):
-        sign = -1 if (i % 2)==0 else +1
-        if i==0 or i==(len(pos)-1):
-            position[i,0] += np.random.normal(0, 0.5)*sqrt_dt + sign*F*np.cos(angle)*dt
-            position[i,1] += np.random.normal(0, 0.5)*sqrt_dt + sign*F*np.sin(angle)*dt
-            plt.plot(pos[i,0], pos[i,1], 'ro', markersize=mks)
-        if i > 0: plt.plot([pos[i-1,0],pos[i,0]], [pos[i-1,1],pos[i,1]], 'gray', linestyle=':', marker='')
-        
-
-
-
-    #position[0,0] += (-1)**random.randint(0,1)*np.random.normal()*np.sqrt(dt) - F*np.cos(angle)*dt
-    #position[0,1] += (-1)**random.randint(0,1)*np.random.normal()*0.4 - F*np.sin(angle)
-    #position[1,0] += (-1)**random.randint(0,1)*np.random.normal()*0.4 + F*np.cos(angle) #+ length*np.cos(angle[0])
-    #position[1,1] += (-1)**random.randint(0,1)*np.random.normal()*0.4 + F*np.sin(angle) #+ length*np.sin(angle[0])
-
-
 def animate(i):
     phi = angle(pos)
     plt.clf()
     plt.axis(( -1*box, box, -1*box, box))
-    pos_update(pos, phi)
-    # plt.plot(pos[0,0], pos[0,1], 'ro', markersize=mks)
-    # plt.plot(pos[1,0], pos[1,1], 'bo', markersize=mks)
-    # plt.plot(pos[:,0], pos[:,1], 'gray', linestyle=':', marker='')
+    F = spring(pos)
+    for i in range(len(pos)):
+        sign = -1 if (i % 2)==0 else +1
+        if i==0 or i==(len(pos)-1):
+            pos[i,0] += np.random.normal(0, 0.5)*sqrt_dt + sign*F*np.cos(phi)*dt
+            pos[i,1] += np.random.normal(0, 0.5)*sqrt_dt + sign*F*np.sin(phi)*dt
+            plt.plot(pos[i,0], pos[i,1], 'ro', markersize=mks)
+        if i > 0: plt.plot([pos[i-1,0],pos[i,0]], [pos[i-1,1],pos[i,1]], 'gray', linestyle=':', marker='')
 
 ani = FuncAnimation(plt.gcf(), animate, interval=100)
 plt.tight_layout()
