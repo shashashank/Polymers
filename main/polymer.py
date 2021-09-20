2ab73af79b1ce58138dc6d77d4cd05a328d706a3
Shashank S K <shashank@mailinator.org>
2021-09-20 14:41:30 +0530

    Make polymer.py parallelization friendly.

--- main/polymer.py
+++ main/polymer.py
@@ -3,6 +3,7 @@ import random
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from numba import prange,jit
+import time
 
 # Generates the force based on the displacement between the two polymer atoms
 @jit(fastmath=True, nopython=True)
@@ -16,9 +17,10 @@ def spring(x, y, length):
 def e2e_distance(p_init, p_fin):
     return np.sqrt((p_fin[0] - p_init[0])*(p_fin[0] - p_init[0]) + (p_fin[1] - p_init[1])*(p_fin[1] - p_init[1]))
 
+# Evolves the points of the polymer due to thermal fluctuation
 @jit(fastmath=True, nopython=True)
 def animate(pos, phi, chain_length, bond_length=2, time_step=1e-3, std_deviation=1):
-    n=chain_length          # Length of the polymer
+    n = chain_length        # Length of the polymer
     length = bond_length    # distance between the two particles
     dt = time_step          # delta_t
     sigma = std_deviation   # std dev of noise term
@@ -43,23 +45,27 @@ def animate(pos, phi, chain_length, bond_length=2, time_step=1e-3, std_deviation
 
     return e2e_distance(pos[0,:], pos[n-1,:])
 
-L = np.zeros(1000)
-#@jit(fastmath=True, nopython=True, parallel = True)
-def yes():
-    length = 2                  # bond length
-    time = int(1e5)                  # simulation time
+
+
+@jit(fastmath=True, parallel = True)
+def yes(array1, array2, time, step, length):
+    L = array1
+    L1 = array2
     x = np.zeros(time)
-    for i in range(len(L)):
+    for i in prange(L.shape[0]):
         n = 10 + i
-        pos = np.zeros([n,2])   # position array
-        phi = np.zeros([1])     # angle
+        pos = np.zeros((n,2))   # position array
+        phi = np.zeros(1)     # angle
 
         # Setting the IC
         for f in range(len(pos)):
             pos[f,0] = f * length
+        
         for j in range(time):
-            x[j] = animate(pos, phi, n, length, 1e-3, 1)
+            x[j] = animate(pos, phi, n, length, step, 1)
+            if j==0: L1[i] = x[j]
         L[i] = np.sum(x)/time
+    return L
         #f = int(np.floor(n/2))
         #box = 20
         # plt.axis((pos[f,0] - box, pos[f,0] + box, pos[f,1] - box, pos[f,1]+ box))
@@ -68,9 +74,19 @@ def yes():
         #     if k > 0: plt.plot([pos[k-1,0],pos[k,0]], [pos[k-1,1],pos[k,1]], 'gray', linestyle=':', marker='')
         # plt.show()
 
-yes()
-plt.plot(L)
-plt.show
+n = 100
+l = 2                  # bond length
+t = int(1e8)           # simulation time
+s = 1e-5               # step length
+L = np.zeros(n)        
+L1 = np.zeros(n)       # shows initial E2E dist
 
+start = time.time()
+y = yes(L, L1, t, s, l)
+x = np.linspace(10, len(y)+10, len(y))
+plt.plot(x, y, 'b')
+plt.plot(x, L1, 'r')
 plt.tight_layout()
 plt.show()
+end = time.time()
+print("Elapsed (after compilation) = %s" % (end - start))
